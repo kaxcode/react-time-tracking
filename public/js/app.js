7c83a0f2d2ba26ac22bb6f20573e0d3a18b02273
Kenia Valladarez <kenia.valladarez@cloudreach.com>
2018-01-10 15:17:40 -0500

    Changes the data to use props.

--- public/js/app.js
+++ public/js/app.js
@@ -56,24 +56,21 @@ class ToggleableTimerForm extends React.Component {
 
 class EditableTimerList extends React.Component {
   render() {
+    const timers = this.props.timers.map((timer) => (
+      <EditableTimer
+        key={timer.id}
+        id={timer.id}
+        title={timer.title}
+        project={timer.elapsed}
+        elapsed={timer.elapsed}
+        runningSince={timer.runningSince}
+      />
+    ));
     return (
       <div id='timers'>
-        <EditableTimer
-          title='Learn React'
-          project='Web Domination'
-          elapsed='8986300'
-          runningSince={null}
-          editFormOpen={false}
-        />
-        <EditableTimer
-          title='Learn extreme ironing'
-          project='World Domination'
-          elapsed='3890985'
-          runningSince={null}
-          editFormOpen={false}
-        />
+       {timers}
       </div>
-    );
+    )
   }
 }
 
