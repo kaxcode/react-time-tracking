18fb9c875556d32bba20f271684051fb210d3791
Kenia Valladarez <kenia.valladarez@cloudreach.com>
2018-01-12 16:33:10 -0500

    Creates the TimerActionButton component.

--- public/js/app.js
+++ public/js/app.js
@@ -330,6 +330,30 @@ class TimerForm extends React.Component {
   }
 }
 
+class TimerActionButton extends React.Component {
+  render() {
+    if (this.props.timerIsRunning) {
+      return (
+        <div
+          className='ui bottom attached red basic button'
+          onClick={this.props.onStopClick}
+        >
+          Stop
+        </div>
+      );
+    } else {
+      return (
+        <div
+          className='ui bottom attached green basic button'
+          onClick={this.props.onStartClick}
+        >
+          Start
+        </div>
+      );
+    }
+  }
+}
+
 ReactDOM.render(
   <TimersDashboard />,
   document.getElementById('content')
