57cd6ded8ff1782b66e8d200897b9d5e1a66aaaf
Kenia Valladarez <kenia.valladarez@cloudreach.com>
2018-01-15 20:54:01 -0500

    Invokes the deleteTimer method.

--- public/js/app.js
+++ public/js/app.js
@@ -67,6 +67,10 @@ class TimersDashboard extends React.Component {
     this.setState({
       timers: this.state.timers.filter(t => t.id !== timerId),
     });
+
+    client.deleteTimer(
+      { id: timerId }
+    )
   };
 
   startTimer = (timerId) => {
